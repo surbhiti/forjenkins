ca4b9ce46316072ebedf1cf21d72a0c54f07c75e
surbhiti <surbhitiwari70@gmail.com>
2023-08-29 08:12:51 +0530

    again push

--- Dummy2/src/test/java/simplepro/Simpleclasstest.java
+++ Dummy2/src/test/java/simplepro/Simpleclasstest.java
@@ -1,14 +1,15 @@
 package simplepro;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Simpleclass {
-
-	public static void main(String[] args) {
+public class Simpleclasstest {
+@Test
+	public  void m2() {
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver =new EdgeDriver();
 		driver.get("http://localhost:8888");
